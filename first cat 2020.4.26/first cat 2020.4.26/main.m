afb1b06143a2f85501375a4773e1e005b5a815a5
HollisWong <HollisWong629@outlook.com>
2020-03-28 16:28:38 +0800

    initial commit

---
+++ first cat 2020.4.26/first cat 2020.4.26/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  first cat 2020.4.26
+//
+//  Created by 黄昊 on 2020/3/28.
+//  Copyright © 2020 黄昊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
